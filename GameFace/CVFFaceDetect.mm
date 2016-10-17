74039f14967f8f89ac4f4273329825d5abd61431
stanchiang <stanchiang23@gmail.com>
2016-10-17 13:16:14 -0700

    simplifying opencv/dlib detection code

--- GameFace/CVFFaceDetect.mm
+++ GameFace/CVFFaceDetect.mm
@@ -34,7 +34,7 @@ using namespace cv;
 CascadeClassifier cascade;
 std::string modelFileNameCString;
 double scale = 1;
-
+dlib::shape_predictor sp;
 @interface CVFFaceDetect() {
     bool _inited;
 }
@@ -46,58 +46,61 @@ double scale = 1;
 -(void)processMat:(cv::Mat)mat
 {
     if (!_inited) {
-//        NSString* haarDataPath =
-//        [[NSBundle mainBundle] pathForResource:@"haarcascade_frontalface_alt.xml" ofType:nil];
-//        
-//        cascade.load([haarDataPath UTF8String]);
+        NSString* haarDataPath =
+        [[NSBundle mainBundle] pathForResource:@"haarcascade_frontalface_alt.xml" ofType:nil];
+        
+        cascade.load([haarDataPath UTF8String]);
         
         NSString *modelFileName = [[NSBundle mainBundle] pathForResource:@"shape_predictor_68_face_landmarks" ofType:@"dat"];
         modelFileNameCString = [modelFileName UTF8String];
+        dlib::deserialize(modelFileNameCString) >> sp;
         
         _inited = true;
     }
 
     cvtColor(mat, mat, CV_BGR2RGB);
-//
-//    int i = 0;
-//    vector<cv::Rect> faces;
-//    const static Scalar colors[] =  { CV_RGB(0,0,255),
-//        CV_RGB(0,128,255),
-//        CV_RGB(0,255,255),
-//        CV_RGB(0,255,0),
-//        CV_RGB(255,128,0),
-//        CV_RGB(255,255,0),
-//        CV_RGB(255,0,0),
-//        CV_RGB(255,0,255)} ;
-//    Mat gray, smallImg( cvRound (mat.rows/scale), cvRound(mat.cols/scale), CV_8UC1 );
-//    
-//    cvtColor( mat, gray, CV_RGB2GRAY );
-//    resize( gray, smallImg, smallImg.size(), 0, 0, INTER_LINEAR );
-//    equalizeHist( smallImg, smallImg );
-//    
-//    cascade.detectMultiScale( smallImg, faces,
-//                             1.2, 2, 0
-//                             //|CV_HAAR_FIND_BIGGEST_OBJECT
-//                             //|CV_HAAR_DO_ROUGH_SEARCH
-//                             |CV_HAAR_SCALE_IMAGE
-//                             ,
-//                             cv::Size(75, 75) );
-//    for( vector<cv::Rect>::const_iterator r = faces.begin(); r != faces.end(); r++, i++ ) {
-//        Scalar color = colors[i%8];
-//        rectangle(mat, r->br(), r->tl(), color);
-//    }
-//    
-    
-    //MARK - DLIB processing
-    auto estimator = HeadPoseEstimation(modelFileNameCString);
-    estimator.focalLength = 500;
-    estimator.opticalCenterX = 360;
-    estimator.opticalCenterY = 640;
 
-    estimator.update(mat,0);
+    int i = 0;
+    vector<cv::Rect> faces;
+    const static Scalar colors[] =  { CV_RGB(0,0,255),
+        CV_RGB(0,128,255),
+        CV_RGB(0,255,255),
+        CV_RGB(0,255,0),
+        CV_RGB(255,128,0),
+        CV_RGB(255,255,0),
+        CV_RGB(255,0,0),
+        CV_RGB(255,0,255)} ;
+    Mat gray, smallImg( cvRound (mat.rows/scale), cvRound(mat.cols/scale), CV_8UC1 );
     
-    for(auto pose : estimator.poses(mat)) {
-        cout << "Head pose: (" << pose.transformation_matrix(0,3) << ", " << pose.transformation_matrix(1,3) << ", " << pose.transformation_matrix(2,3) << ")" << endl;
+    cvtColor( mat, gray, CV_RGB2GRAY );
+    resize( gray, smallImg, smallImg.size(), 0, 0, INTER_LINEAR );
+    equalizeHist( smallImg, smallImg );
+    
+    cascade.detectMultiScale( smallImg, faces,
+                             1.2, 2, 0
+                             //|CV_HAAR_FIND_BIGGEST_OBJECT
+                             //|CV_HAAR_DO_ROUGH_SEARCH
+                             |CV_HAAR_SCALE_IMAGE
+                             ,
+                             cv::Size(75, 75) );
+    
+    
+    for( vector<cv::Rect>::const_iterator r = faces.begin(); r != faces.end(); r++, i++ ) {
+        dlib::cv_image<dlib::bgr_pixel> dlibMat(mat);
+        
+//        for converting either direction use this http://stackoverflow.com/a/34873134/1079379
+//        static cv::Rect dlibRectangleToOpenCV(dlib::rectangle r){return cv::Rect(cv::Point2i(r.left(), r.top()), cv::Point2i(r.right() + 1, r.bottom() + 1));}
+//        static dlib::rectangle openCVRectToDlib(cv::Rect r){return dlib::rectangle((long)r.tl().x, (long)r.tl().y, (long)r.br().x - 1, (long)r.br().y - 1);}
+        
+        dlib::rectangle dlibRect((long)r->tl().x, (long)r->tl().y, (long)r->br().x - 1, (long)r->br().y - 1);
+//        dlib::draw_rectangle(dlibMat, dlibRect, dlib::rgb_pixel(0, 255, 255));
+        
+        dlib::full_object_detection shape = sp(dlibMat, dlibRect);
+        
+        for (unsigned long k = 0; k < shape.num_parts(); k++) {
+            draw_solid_circle(dlibMat, shape.part(k), 3, dlib::rgb_pixel(0, 255, 255));
+        }
+        
     }
     
     [self matReady:mat];
