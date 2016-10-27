92f1e6c84a919fa28e4a80458b80412246b93054
stanchiang <stanchiang23@gmail.com>
2016-10-26 17:53:49 -0700

    removed unused dlibwrapper code

--- GameFace/gameface-Bridging-Header.h
+++ GameFace/gameface-Bridging-Header.h
@@ -2,9 +2,6 @@
 //  Use this file to import your target's public headers that you would like to expose to Swift.
 //
 
-#import "DlibWrapper.h"
-#import "DlibWrapperDelegate.h"
-
 #import "CVFImageProcessor.h"
 #import "CVFImageProcessorDelegate.h"
 
